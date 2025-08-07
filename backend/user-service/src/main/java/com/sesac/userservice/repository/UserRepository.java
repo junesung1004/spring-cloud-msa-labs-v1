88abd6152b572a9338fab14488eb71823a9c7b07
GitHub Action <actions@github.com>
2025-08-07 17:15:32 +0900

    feat: init

--- backend/user-service/src/main/java/com/sesac/userservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sesac.userservice.repository;
-
-import com.sesac.userservice.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
---
+++ config-server-2/src/test/java/com/sesac/configserver2/ConfigServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesac.configserver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
