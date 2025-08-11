b3b0f68dea51102b81354ab0d432fc189aa6a51d
GitHub Action <actions@github.com>
2025-08-11 14:20:04 +0900

    faet: 유저 서비스의 JWT 로그인 기능 구현

--- backend/user-service/src/main/java/com/sesac/userservice/repository/UserRepository.java
+++ backend/user-service/src/main/java/com/sesac/userservice/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.sesac.userservice.repository;
 
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import com.sesac.userservice.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
 }
