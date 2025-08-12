60ed4829f9f64a1dff0cd68820c68b47f02f4fbc
GitHub Action <actions@github.com>
2025-08-12 12:42:42 +0900

    feat: resilience4j 회복성 패턴 추가

--- backend/order-service/src/main/java/com/sesac/orderservice/facade/UserServiceFacade.java
+++ backend/order-service/src/main/java/com/sesac/orderservice/facade/UserServiceFacade.java
@@ -1,13 +1,12 @@
 package com.sesac.orderservice.facade;
 
-import org.springframework.stereotype.Service;
-
 import com.sesac.orderservice.client.UserServiceClient;
 import com.sesac.orderservice.client.dto.UserDto;
-
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -17,16 +16,16 @@ public class UserServiceFacade {
 	private final UserServiceClient userServiceClient;
 
 	@CircuitBreaker(name = "user-service", fallbackMethod = "getUserFallback")
+	@Retry(name = "user-service")
 	public UserDto getUserWithFallback(Long userId) {
 		log.info("User service 호출 시도 - userId = {}", userId);
 		return userServiceClient.getUserById(userId);
 	}
 
-
-	// Fallback 메서드 : User Service 장애 시 기본 사용자 정보 반환
+	// Fallback 메서드: User Service 장애 시 기본 사용자 정보 반환
 	public UserDto getUserFallback(Long userId, Throwable ex) {
 
-		log.warn("User service 장애 감지! Fallback 실행 - userId: {}, 에러: {}", userId, ex.getMessage());
+		log.warn("User Service 장애 감지! Fallback 실행 - userId: {}, 에러: {}", userId, ex.getMessage());
 
 		UserDto defaultUser = new UserDto();
 		defaultUser.setId(userId);
--- backend/order-service/src/main/java/com/sesac/orderservice/service/OrderService.java
+++ backend/order-service/src/main/java/com/sesac/orderservice/service/OrderService.java
@@ -1,11 +1,9 @@
 package com.sesac.orderservice.service;
 
 
-import java.math.BigDecimal;
-import java.util.List;
 
 import com.sesac.orderservice.client.ProductServiceClient;
-import com.sesac.orderservice.client.UserServiceClient;
+// import com.sesac.orderservice.client.UserServiceClient;
 import com.sesac.orderservice.client.dto.ProductDto;
 import com.sesac.orderservice.client.dto.UserDto;
 import com.sesac.orderservice.dto.OrderRequestDto;
@@ -13,10 +11,11 @@ import com.sesac.orderservice.entity.Order;
 import com.sesac.orderservice.facade.UserServiceFacade;
 import com.sesac.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,45 +23,40 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    // private final UserServiceClient userServiceClient;
-    private final ProductServiceClient productServiceClient;
     private final UserServiceFacade userServiceFacade;
+    private final ProductServiceClient productServiceClient;
+
 
     public Order findById(Long id) {
         return orderRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Order not found with id: " + id)
+            () -> new RuntimeException("Order not found with id: " + id)
         );
     }
 
-
-    // 주문 생성 ( 고객이 주문했을 때 )
+    // 주문 생성 (고객이 주문했을 때)
     @Transactional
     public Order createOrder(OrderRequestDto request) {
-        // 사용자 정보 가져오기
+
         UserDto user = userServiceFacade.getUserWithFallback(request.getUserId());
+        if (user == null) throw new RuntimeException("User not found");
 
-        if(user == null) throw new RuntimeException("User not found");
-
-        // 상품 정보 가져오기
         ProductDto product = productServiceClient.getProductById(request.getProductId());
-        if(product == null) throw new RuntimeException("Product not found");
+        if (product == null) throw new RuntimeException("Product not found");
 
-        if(product.getStockQuantity() < request.getQuantity()) {
-            throw new RuntimeException("재고 부족");
+        if (product.getStockQuantity() < request.getQuantity()) {
+            throw new RuntimeException("Out of stock");
         }
 
-        // Order 주문
         Order order = new Order();
-        order.setUserId(user.getId());
+        order.setUserId(request.getUserId());
         order.setTotalAmount(product.getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
         order.setStatus("COMPLETED");
-        orderRepository.save(order);
-        return order;
 
+        return orderRepository.save(order);
     }
-
 
     public List<Order> getOrdersByUserId(Long userId) {
         return orderRepository.findByUserIdOrderByCreatedAtDesc(userId);
     }
+
 }
